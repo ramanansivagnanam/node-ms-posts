4dc2e99b3fb4952b336fc84030467bbe91674b13
Ramanan S <rams2@publicisgroupe.net>
2021-12-28 20:02:32 +0530

    connected between services

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
+
 
 const app = express();
 app.use(bodyParser());
@@ -13,12 +15,21 @@ app.get('/posts',(req, res) => {
     res.status(200).send(posts);
 });
 
-app.post('/createpost', (req, res) => {
+app.post('/createpost', async (req, res) => {
     const { title } = req.body;
     const id = randomBytes(4).toString('hex');
-    const dataMap = { id, title};
+    const dataMap = { id, title}; 
     posts.push(dataMap);
+    // emit event from here to event bus service.
+    await axios.post('http://localhost:6000/events',{
+        type: 'POST_CREATED',
+        payload: { dataMap }
+    });
     res.status(201).send(dataMap);
+});
+
+app.post('/events', (req, res) => {
+    console.log(req.body);
 })
 
 app.listen(4000, ()=> {
